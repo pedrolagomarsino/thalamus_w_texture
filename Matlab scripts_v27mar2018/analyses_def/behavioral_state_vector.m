67058d10b7b9e725e0cef910fdea7c03e37e292e
Pedro Lagomarsino <pedro.lagomarsino@iit.it>
2018-10-26 15:33:40 +0200

    add the counts related to texture in the behavioural_state_vector function without modifying state_vector

--- Matlab scripts_v27mar2018/analyses_def/behavioral_state_vector.m
+++ Matlab scripts_v27mar2018/analyses_def/behavioral_state_vector.m
@@ -7,7 +7,6 @@ if params.stimulus == 0
     numFramesW = sum( data.whisking == 1 )-numFramesWL;
     numFramesL = sum( data.locomotion == 1 )-numFramesWL;
     
-       
     %build states count vector
     statesCount_Q_W_WL_L = [numFramesQuiet, numFramesW, numFramesWL, numFramesL];
     statesFreq_Q_W_WL_L = statesCount_Q_W_WL_L/length(data.time_ca);
@@ -20,5 +19,23 @@ if params.stimulus == 0
     behavior.states_vector = state_vector;
        
 else
+    numFramesQuiet = sum( data.whisking + data.locomotion + data.stimulus == 0 );
+    numFramesWLT = sum( data.whisking + data.locomotion + data.stimulus == 3 );
+    numFramesWL = sum( data.whisking + data.locomotion == 2 ) - numFramesWLT;
+    numFramesWT = sum( data.whisking + data.stimulus == 2 ) - numFramesWLT;
+    numFramesLT = sum( data.stimulus + data.locomotion == 2 ) - numFramesWLT;
+    numFramesW = sum( data.whisking == 1 ) - numFramesWL - numFramesWT - numFramesWLT;
+    numFramesL = sum( data.locomotion == 1 ) - numFramesWL - numFramesLT - numFramesWLT;
+    numFramesT = sum( data.stimulus == 1 ) - numFramesWT - numFramesLT - numFramesWLT;
     
+    %build states count vector
+    statesCount_Q_W_T_WL_WT_WLT_LT_L = [numFramesQuiet, numFramesW, numFramesT, numFramesWL, numFramesWT, numFramesWLT, numFramesLT, numFramesL];
+    statesFreq_Q_W_T_WL_WT_WLT_LT_L = statesCount_Q_W_T_WL_WT_WLT_LT_L/length(data.time_ca);
+    behavior.count = statesCount_Q_W_T_WL_WT_WLT_LT_L;
+    behavior.freq = statesFreq_Q_W_T_WL_WT_WLT_LT_L;
+    
+    %build states vector
+    state_vector = zeros(length(data.time_ca),1);
+    state_vector = state_vector + data.whisking + 2*data.locomotion;
+    behavior.states_vector = state_vector;
 end
