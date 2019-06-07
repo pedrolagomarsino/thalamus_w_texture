f4e599d6e8c6d5fd65b0ebe417c2bf782b57010a
Pedro Lagomarsino <pedrolagomarsinor@gmail.com>
2019-06-07 16:14:03 +0200

    added the loop over all cells to build the matrices to cluster

--- clustering.py
+++ clustering.py
@@ -2,12 +2,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 import h5py
 import scipy.signal as sig
+from sklearn.cluster import KMeans
+
 file_name = 'C:/Users/plagomarsino/work/Thalamus_proyect/Whisking on Texture/\
 Data/20180412/Mouse_2314/TSeries-04122018-0853-1003/Post_proc_2/TS1003_C_no_stim_times.mat'
 f = h5py.File(file_name,'r')
 trace_all = f.get('data/C_df')
 period = f.get('data/framePeriod')[0]
-trace = trace_all[:,1]
+trace = trace_all[:,0]
 
 def find_events(trace,period):
     """
@@ -74,6 +76,7 @@ def find_events(trace,period):
     events['n_peaks'] = len(peaks)
 
     return events
+
 def event_analysis(trace,events,period):
     """
     Calculate the values that characterize the events
@@ -90,14 +93,15 @@ def event_analysis(trace,events,period):
     decay_time: array with the time in seconds from the peak_amp to the end of the event
     AUC_event: area under the curve of the event (trapezoid integration of the signal for the duration of the event)
     """
-    init = (events['event_init']/period).astype(int)
-    fin = (events['event_end']/period).astype(int)
+    init = np.around((events['event_init']/period)).astype(int)
+    fin = np.around((events['event_end']/period)).astype(int)
     peak_amp = np.array([peak for j,_ in enumerate(init) for peak in np.nditer(max(trace[init[j]:fin[j]]))])
     rise_time = np.array([time*period[0] for i,peak in enumerate(peak_amp) for time in (np.where(trace==peak)[0]-init[i])])
     decay_time = np.array([time*period[0] for i,peak in enumerate(peak_amp) for time in (fin[i]-np.where(trace==peak)[0])])
     AUC_event = np.array([area for i,_ in enumerate(init) for area in np.nditer(np.trapz(trace[init[i]:(fin[i]+1)],dx=period))])
 
     return [peak_amp,rise_time,decay_time,AUC_event]
+
 def peak_analysis(trace,events,period):
     """
     Same as in event analysis but for each peak above 1 std. begining and end
@@ -105,53 +109,61 @@ def peak_analysis(trace,events,period):
     """
     first_flag = False
     last_flag  = False
-    peaks = (events['peaks']['time']/period).astype(int)
-    valleys = (events['valleys']['time']/period).astype(int)
+    peaks = np.around((events['peaks']['time']/period)).astype(int)
+    valleys = np.around((events['valleys']['time']/period)).astype(int)
     extremes = np.sort(np.concatenate((peaks,valleys)))
-
     if extremes[0] == peaks[0]:
         first_flag = True
-
     if extremes[-1] == peaks[-1]:
         last_flag = True
 
-    init = [extremes[idx-1] for peak in peaks for idx in np.where(extremes==peak)[0] if idx>0]
-    end = [extremes[idx+1] for peak in peaks for idx in np.where(extremes==peak)[0] if idx<len(extremes)-1]
+    init = [extremes[idx-1] for peak in peaks for idx in np.where(extremes==peak)[0] if idx>0 if idx<len(extremes)-1]
+    end = [extremes[idx+1] for peak in peaks for idx in np.where(extremes==peak)[0] if idx>0 if idx<len(extremes)-1]
+    peaks_time = events['peaks']['time']
+    peaks_val =events['peaks']['value']
     if first_flag:
-        peak_amp = events['peaks']['value'][1:]-trace[init]
-        rise_time = events['peaks']['time'][1:]-init*period
-    else:
-        peak_amp = events['peaks']['value']-trace[init]
-        rise_time = events['peaks']['time']-init*period
+        peaks_time = peaks_time[1:]
+        peaks_val = peaks_val[1:]
     if last_flag:
-        deca_time = end*period-events['peaks']['time'][:-1]
-    else:
-        deca_time = end*period-events['peaks']['time']
+        peaks_time = peaks_time[:-1]
+        peaks_val = peaks_val[:-1]
 
-    # for i = 1:n
-        # init_val = find(events.valleys.time < events.peaks.time(i),1,'last');
-        # end_val  = find(events.valleys.time > events.peaks.time(i),1);
-        
-#
-        # if ~isempty(init_val) && ~isempty(end_val)
-            # peak_amp(i) = events.peaks.value(i)-events.valleys.value(init_val);
-
-#
-            # rise_time(i) = events.peaks.time(i)-events.valleys.time(init_val);
-
-#
-            # decay_time(i) = events.valleys.time(end_val)-events.peaks.time(i);
-
-#
-            # AUC_peak(i) = trapz(time(round(events.valleys.time(init_val)/period:events.valleys.time(end_val)/period)),cal_trace(round(events.valleys.time(init_val)/period:events.valleys.time(end_val)/period)));
-        # end
-    # end
-    # peak_amp(peak_amp==0)     = [];
-    # rise_time(rise_time==0)   = [];
-    # decay_time(decay_time==0) = [];
+    peak_amp = peaks_val-trace[init]
+    rise_time = peaks_time-init*period
+    decay_time = end*period-peaks_time
+    AUC_peak = np.array([area for i,_ in enumerate(init) for area in np.nditer(np.trapz(trace[init[i]:(end[i]+1)],dx=period))])
 
     return [peak_amp,rise_time,decay_time,AUC_peak]
 
+def find_increments(trace,period):
+    """
+    Finds the increment in calcium signal for each period of increasing intensity.
+    """
+    delta_intensity = np.ediff1d(trace,to_begin=0)
+    increments_time = [i*period[0] for i,v in enumerate(delta_intensity) if v>0]#(find(delta_intensity>0)*period)
+    increments_value = [v for i,v in enumerate(delta_intensity) if v>0]# increments_value = delta_intensity(delta_intensity>0)';
+    return [increments_time,increments_value]
 
-events = find_events(trace,period)
-[peak_amp,rise_time,decay_time,AUC_event] = event_analysis(trace,events,period)
+events = find_events(trace_all[:,0],period)
+events_space = np.transpose(np.array(event_analysis(trace_all[:,0],events,period)))
+peaks_space = np.transpose(np.array(peak_analysis(trace_all[:,0],events,period)))
+increments_time, increments_value = np.array(find_increments(trace_all[:,0],period))
+for i in range(1,len(trace_all[0])):
+    events = find_events(trace_all[:,i],period)
+    events_space = np.concatenate((events_space, np.transpose(np.array(event_analysis(trace_all[:,i],events,period)))),axis=0)
+    peaks_space = np.concatenate((peaks_space, np.transpose(np.array(peak_analysis(trace_all[:,i],events,period)))),axis=0)
+    increments_time,increments_value = np.concatenate(([increments_time,increments_value],\
+    np.array(find_increments(trace_all[:,i],period))),axis=1)
+#plt.hist(increments_value,100)
+# X = np.transpose(np.array(events_space))#[events_space[0],events_space[1]]
+# np.shape(X)
+# kmeans = KMeans(n_clusters=2).fit(X)
+# np.shape(kmeans.cluster_centers_)
+# events_space = np.array(events_space)
+# whos(events_space)
+# whos
+# np.shape(events_space[3])
+# pepe=np.array([[3,2,1,2,2]])
+# pepes = np.concatenate((pepe,pepe),axis=0)
+# teta = np.concatenate((pepes,pepe),axis=0)
+# np.shape(teta)
