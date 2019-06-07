9d4a5d76b3c017690d4e63e3e5886e697e328ee9
Pedro Lagomarsino <pedrolagomarsinor@gmail.com>
2019-06-07 14:07:19 +0200

    added the function peak_analysis and find_increments to the python translated code for the clustering

--- clustering.py
+++ clustering.py
@@ -90,8 +90,6 @@ def event_analysis(trace,events,period):
     decay_time: array with the time in seconds from the peak_amp to the end of the event
     AUC_event: area under the curve of the event (trapezoid integration of the signal for the duration of the event)
     """
-    n = events['n_events'];
-    time = np.array(range(0,len(trace)))*period
     init = (events['event_init']/period).astype(int)
     fin = (events['event_end']/period).astype(int)
     peak_amp = np.array([peak for j,_ in enumerate(init) for peak in np.nditer(max(trace[init[j]:fin[j]]))])
@@ -100,6 +98,60 @@ def event_analysis(trace,events,period):
     AUC_event = np.array([area for i,_ in enumerate(init) for area in np.nditer(np.trapz(trace[init[i]:(fin[i]+1)],dx=period))])
 
     return [peak_amp,rise_time,decay_time,AUC_event]
+def peak_analysis(trace,events,period):
+    """
+    Same as in event analysis but for each peak above 1 std. begining and end
+    are defined as the previous and following valley for each peak
+    """
+    first_flag = False
+    last_flag  = False
+    peaks = (events['peaks']['time']/period).astype(int)
+    valleys = (events['valleys']['time']/period).astype(int)
+    extremes = np.sort(np.concatenate((peaks,valleys)))
+
+    if extremes[0] == peaks[0]:
+        first_flag = True
+
+    if extremes[-1] == peaks[-1]:
+        last_flag = True
+
+    init = [extremes[idx-1] for peak in peaks for idx in np.where(extremes==peak)[0] if idx>0]
+    end = [extremes[idx+1] for peak in peaks for idx in np.where(extremes==peak)[0] if idx<len(extremes)-1]
+    if first_flag:
+        peak_amp = events['peaks']['value'][1:]-trace[init]
+        rise_time = events['peaks']['time'][1:]-init*period
+    else:
+        peak_amp = events['peaks']['value']-trace[init]
+        rise_time = events['peaks']['time']-init*period
+    if last_flag:
+        deca_time = end*period-events['peaks']['time'][:-1]
+    else:
+        deca_time = end*period-events['peaks']['time']
+
+    # for i = 1:n
+        # init_val = find(events.valleys.time < events.peaks.time(i),1,'last');
+        # end_val  = find(events.valleys.time > events.peaks.time(i),1);
+        
+#
+        # if ~isempty(init_val) && ~isempty(end_val)
+            # peak_amp(i) = events.peaks.value(i)-events.valleys.value(init_val);
+
+#
+            # rise_time(i) = events.peaks.time(i)-events.valleys.time(init_val);
+
+#
+            # decay_time(i) = events.valleys.time(end_val)-events.peaks.time(i);
+
+#
+            # AUC_peak(i) = trapz(time(round(events.valleys.time(init_val)/period:events.valleys.time(end_val)/period)),cal_trace(round(events.valleys.time(init_val)/period:events.valleys.time(end_val)/period)));
+        # end
+    # end
+    # peak_amp(peak_amp==0)     = [];
+    # rise_time(rise_time==0)   = [];
+    # decay_time(decay_time==0) = [];
+
+    return [peak_amp,rise_time,decay_time,AUC_peak]
+
 
 events = find_events(trace,period)
 [peak_amp,rise_time,decay_time,AUC_event] = event_analysis(trace,events,period)
